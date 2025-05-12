54f71e6c4a2951d63d63db4b008362655862f526
Harshal <hraj1137@gmail.com>
2025-05-12 18:59:55 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,9 +56,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: "var(--radius) - 4px",
+        md: "calc(var(--radius) - 8px)",
+        sm: "calc(var(--radius) - 10px)",
       },
       keyframes: {
         "accordion-down": {
